da759a6c8886e6929711eda49276eca9910f696e
MZ <maria.zhukova.vrn@gmail.com>
2017-11-03 15:59:27 +0300

    new projects added

---
+++ Conso/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
