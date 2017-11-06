cd41cd1011e7613ee27931c52e4f5d9236c11b85
ZhukovaMariia <vesnushka_only@mail.ru>
2017-11-06 20:44:27 +0300

    news

---
+++ myProjects/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myProjects
+{
+    public class Person
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+    }
+}
--- myProjects/Program.cs
+++ myProjects/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,35 @@ namespace myProjects
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            List<string> personsList = new List<string>();
+            Hashtable hashtable = new Hashtable();
+            Person personV = new Person("Василий", 32);
+            Person personB = new Person("Борис", 25);
+            Person personL = new Person("Людмила", 27);
+
+            hashtable.Add("Василий", 32);
+            hashtable.Add("Борис", 25);
+            hashtable.Add("Людмила", 27);
+
+
+            var result =
+                from string name in hashtable.Keys
+                where (int)hashtable[name]> 25
+                orderby name
+                select name;
+
+            var r2 =
+                from int age in hashtable.Values
+                orderby hashtable[age] descending
+                select age;
+
+            var youngest = r2.First<int>();
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
