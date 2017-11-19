3c48e1614175f85c7a882d01109428bb5dd9cd42
ZhukovaMariia <vesnushka_only@mail.ru>
2017-11-19 11:52:01 +0300

    MVC logik added for lessons

---
+++ MVC/Controllers/LessonController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class LessonController : Controller
+    {
+        private MVCContext db = new MVCContext();
+        private static List<LessonItemViewModel> lessons = null;   
+      
+
+        public LessonController()
+        {
+            lessons = new List<LessonItemViewModel>
+            {
+            new LessonItemViewModel { ID = 1, Group = "English Basic", Room = "3.301", Trainer = "Татьяна Борисовна", HomeWork = "1книга 5,6,7 упражнение",DateTime=DateTime.Now },
+            new LessonItemViewModel { ID = 2, Group = "English Elementary", Room = "3.312", Trainer = "Татьяна Борисовна", HomeWork = "Граматика 3 упражнение",DateTime=DateTime.Now }
+            };
+        }
+
+        // GET: LessonItemViewModels
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(lessons);
+            
+        }
+
+        [HttpGet]
+        public ActionResult Lessons()
+        {
+            return View(lessons);
+        }
+
+        // GET: LessonItemViewModels/Details/5
+        [HttpGet]
+        [ActionName("Details")]
+        public ActionResult Details(int id)
+        {
+
+            ViewBag.ViewType = "View";
+            return GetLesson(id);
+         
+        }
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public ActionResult EditLesson(int id)
+        {
+            ViewBag.ViewType = "Edit";
+            return GetLesson(id);
+        }
+
+        [HttpPost]
+        [ActionName ("Edit")]
+        public ActionResult EditLesson(LessonItemViewModel lesson)
+        {
+            ViewBag.ViewType("Edit");
+
+            if (!ModelState.IsValid)
+                return View("Details", lesson);
+            return RedirectToAction("Index");
+
+
+        }
+
+        [HttpGet]
+        [ActionName("Create")]
+        public ActionResult CreateLesson()
+        {
+            ViewBag.ViewType = "Create";
+            var lesson = new LessonItemViewModel
+            {
+                ID = lessons.Max(l => l.ID) + 1,
+                DateTime = DateTime.Now
+            };
+           
+            return View("Details",lesson);
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult CreateLesson(LessonItemViewModel lesson)
+        {
+            ViewBag.ViewType("Create");
+
+            if (!ModelState.IsValid)
+                return View("Details", lesson);
+
+            lesson.ID = lessons.Max(l => l.ID) + 1;
+            lesson.DateTime = DateTime.Now;
+            lessons.Add(lesson);
+            return RedirectToAction("Index");
+
+
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var lesson = lessons.FirstOrDefault(l => l.ID == id);
+            if (lesson == null)
+                return RedirectToAction("Index");
+            lessons.RemoveAt(id);
+            return View("Index",lessons);
+        }
+
+        private ActionResult GetLesson(int id)
+        {
+            var lesson = lessons.FirstOrDefault(l => l.ID == id);
+            if (lesson == null)
+                return RedirectToAction("Index");
+            return View("Details",lesson);
+
+        }
+        // GET: LessonItemViewModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC/Models/LessonItemViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Models
+{
+    public class LessonItemViewModel
+    {
+        [HiddenInput()]
+        public int ID { get; set; }
+        [Description("Название Группы")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Указание группы обязательно")]
+        public string Group { get; set; }
+
+        [Description("Номер комнаты")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Указание комнаты обязательно")]
+        public string Room { get; set; }
+
+        [Description("Преподаватель")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Указание имени преаодавателя обязательно")]
+        public string Trainer { get; set; }
+
+        [Description("Домашнее задание")]
+        [Required(AllowEmptyStrings = true)]
+        [StringLength(250,ErrorMessage ="Превышено максимальое колличество символов")]
+        public string HomeWork { get; set; }
+
+        [Description("Дата занятия")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Указание даты обязательно")]
+        public DateTime DateTime { get; set; }
+    }
+}
