c0e1343a7844231db7a341df1ec76a8ed2a9aa29
MZ <maria.zhukova.vrn@gmail.com>
2018-05-10 15:00:12 +0300

    NewProjects

---
+++ Async_await/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async_await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayResultAsync();
+            ResultOfCycle();
+            Console.ReadLine();
+        }
+        static async void DisplayResultAsync()
+        {
+            int num = 5;
+
+            int result = await FactorialAsync(num);
+            var result2 = await ResultOfCycle();
+            Thread.Sleep(3000);
+            Console.WriteLine("Факториал числа {0} равен {1}", num, result);
+            Console.WriteLine("2={0}", result2);
+              }
+
+        static Task<int> FactorialAsync(int x)
+        {
+            int result = 1;
+
+            return Task.Run(() =>
+            {
+                for (int i = 1; i <= x; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            });
+        }
+       static async Task SomeCycleAsync()
+        {
+            var myTask = await ResultOfCycle();
+
+       }
+        static Task<int> ResultOfCycle()
+        {
+            int sum = 0;
+
+            return Task.Run(() =>
+            {
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            });
+            
+        }
+    }
+}
---
+++ Delegat/BookTracker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegat
+{
+    public delegate void AddBookCallback(int i);
+    class BookTracker
+    {
+        List<Book> books = new List<Book>();
+        public void AddBook(string name, AddBookCallback callback)
+        {
+            books.Add(new Book(name));
+            callback(books.Count);
+        }
+    }
+    public class Book
+    {
+        string Name { get; set; }
+        public Book(string name) { Name = name; }
+
+        BookTracker bookTracker = new BookTracker();
+        public void Add(string name)
+        {
+            this.Name = name;
+            bookTracker.AddBook(Name, delegate (int i)
+             {
+                 i++;
+             });
+            
+            
+        }
+
+
+    }
+}
