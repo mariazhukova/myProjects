c42f59f3ec7a32997de534d3b5aa86988a073348
MZ <maria.zhukova.vrn@gmail.com>
2018-06-17 13:50:53 +0300

    yield example was added

---
+++ yeldExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yeldExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var i in Power(2, 10))
+                Console.WriteLine("exponent is {0}", i);
+            Console.ReadKey();
+
+        }
+
+        private static IEnumerable<int> Power(int el, int exp)
+        {
+            for (int i = 1; i < exp; i++)
+                yield return el * i;
+
+        }
+    }
+}
