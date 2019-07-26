58ff025d1f7780313a2d95c97ef676a7ee894529
MZ <maria.zhukova.vrn@gmail.com>
2019-07-26 17:12:47 +0300

    LinkedList part 1

--- buffetTest/HashTable.cs
+++ buffetTest/HashTable.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace buffetTest
 {
+    /// <summary>
+    /// this code implements theoretical methods 
+    /// how HashTable works under the hood
+    /// </summary>
     class HashTable
     {
         private List<KeyValuePair<int, KeyValuePair<string,int>>> data;
@@ -28,6 +32,13 @@ namespace buffetTest
             KeyValuePair<string, int> obj = new KeyValuePair<string, int>(key, value);
             var h = hash(key);
             data.Add(new KeyValuePair<int, KeyValuePair<string, int>>(h,obj));
+        }
+
+        public IEnumerable<string> keys()
+        {
+            if (data.Count > 0)
+                return data.Select(el => el.Value.Key);
+            else return null;
         }
 
         public int getValue(string key)
---
+++ buffetTest/MyLinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buffetTest
+{
+    class MyLinkedList
+    {
+        LinkedListElement _head { get; set; }
+        LinkedListElement _tail { get; set; }
+        int Lenght { get; set; }
+        public MyLinkedList(string value)
+        {
+            LinkedListElement head = new LinkedListElement();
+            head.Value = value;
+            head.Pointe = null;
+            _head = head;
+            _tail = head;
+            Lenght = 1;
+
+        }
+    }
+    public class LinkedListElement
+    {
+        public string Value { get; set; }
+        public object Pointe { get; set; }
+    }
+}
