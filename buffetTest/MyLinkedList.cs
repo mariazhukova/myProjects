3d20f188ac4127a3898c74171e62b4301cfb2756
MZ <maria.zhukova.vrn@gmail.com>
2019-07-27 16:14:54 +0300

    one way linked list finished

--- buffetTest/MyLinkedList.cs
+++ buffetTest/MyLinkedList.cs
@@ -10,21 +10,87 @@ namespace buffetTest
     {
         LinkedListElement _head { get; set; }
         LinkedListElement _tail { get; set; }
-        int Lenght { get; set; }
+        public int Lenght { get; private set; }
         public MyLinkedList(string value)
         {
             LinkedListElement head = new LinkedListElement();
             head.Value = value;
-            head.Pointe = null;
+            head.Pointer = null;
             _head = head;
             _tail = head;
             Lenght = 1;
 
         }
+
+        public void Append(string value)
+        {
+            var newelement = new LinkedListElement() { Value = value, Pointer = null };
+            this._head.Pointer = newelement;
+            this._tail = newelement;
+            Lenght++;
+        }
+
+        public void Prepend(string value)
+        {
+            var newelement = new LinkedListElement()
+            {
+                Value = value,
+                Pointer = this._head
+            };
+            this._head = newelement;
+            Lenght++;
+        }
+
+        public List<string> PrintValues()
+        {
+            List<string> printer = new List<string>(Lenght);
+            var element = new LinkedListElement()
+            {
+                Value = this._head.Value,
+                Pointer = this._head.Pointer
+            };
+            var next = new LinkedListElement();
+            
+            while (next != null )
+            {
+                printer.Add(element.Value);
+                next = (LinkedListElement)element.Pointer;
+                element.Value = next?.Value;
+                element.Pointer = next?.Pointer;
+            }
+            return printer;
+        }
+
+        public void Insert(string pointer,  string value)
+        {
+            var nextEl = this._head;
+            var previusEl = this._head;
+            while(nextEl.Value != pointer)
+            {
+                previusEl = nextEl;
+                nextEl = (LinkedListElement)nextEl.Pointer;
+            }
+            previusEl.Pointer = new LinkedListElement()
+            { Value = value, Pointer = nextEl };
+            Lenght--;
+        }
+
+        public void Remove(string pointer)
+        {
+            var nextEl = this._head;
+            var previusEl = this._head;
+            while (nextEl.Value != pointer)
+            {
+                previusEl = nextEl;
+                nextEl = (LinkedListElement)nextEl.Pointer;
+            }
+            previusEl.Pointer = (LinkedListElement)nextEl.Pointer;
+            Lenght++;
+        }
     }
     public class LinkedListElement
     {
         public string Value { get; set; }
-        public object Pointe { get; set; }
+        public object Pointer { get; set; }
     }
 }
--- buffetTest/Program.cs
+++ buffetTest/Program.cs
@@ -21,7 +21,17 @@ namespace Algoritms
             int[] array3 = { 1, 2, 2, 3, 4, 1, 5};
             GoogleQuestions gq = new GoogleQuestions();
             var result = gq.GetFirstDublicateNested(array3);
-            
+            MyLinkedList myLinkedList = new MyLinkedList("first");
+            myLinkedList.Append("second");
+            myLinkedList.Prepend("zero");
+            myLinkedList.Insert("first", "newElement");
+            var values = myLinkedList.PrintValues();
+            values.ForEach(el => Console.Write(el + " / "));
+            myLinkedList.Remove("newElement");
+            var valuesafter = myLinkedList.PrintValues();
+            Console.WriteLine();
+            valuesafter.ForEach(el => Console.Write(el + " / "));
+            Console.WriteLine();
             Console.WriteLine(result);
  
             Console.ReadKey();
