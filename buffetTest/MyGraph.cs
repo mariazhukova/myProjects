f99b7bbd21c049bd64d47472cd3e7c458dad0652
MZ <maria.zhukova.vrn@gmail.com>
2019-08-02 17:39:59 +0300

    Graph node created

---
+++ buffetTest/MyGraph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buffetTest
+{
+    class MyGraph
+    {
+    }
+
+    public class GraphNode
+    {
+        KeyValuePair<int, List<int>> node;
+
+        public int value { get => node.Key; set { node = new KeyValuePair<int, List<int>>(value, new List<int>()); } }
+        public int edge { set { node.Value.Add(value); } }
+
+        public List<int> GetAllEdges => node.Value;
+
+    }
+}
