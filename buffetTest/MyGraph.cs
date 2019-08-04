2d0224a4c404ef3b0425be2f62c44586e1acb07b
MZ <maria.zhukova.vrn@gmail.com>
2019-08-04 09:26:20 +0300

    Graph compleated

--- buffetTest/MyGraph.cs
+++ buffetTest/MyGraph.cs
@@ -8,6 +8,39 @@ namespace buffetTest
 {
     class MyGraph
     {
+        Dictionary<int, List<int>> adjacencyList;
+        int Count { get => adjacencyList.Count; }
+        public MyGraph()
+        {
+            adjacencyList = new Dictionary<int, List<int>>();
+        }
+
+        public void addNode(int node)
+        {
+            if(!adjacencyList.ContainsKey(node))
+                adjacencyList.Add(node, new List<int>());
+        }
+        public void addEdge(int parentNode, int childNode)
+        {
+            if (adjacencyList.ContainsKey(parentNode))
+                adjacencyList.ElementAt(parentNode).Value.Add(childNode);
+            else addNode(parentNode);
+            if (adjacencyList.ContainsKey(childNode))
+                adjacencyList.ElementAt(childNode).Value.Add(parentNode);
+
+        }
+
+        public IEnumerable<string> ShowConnections()
+        {
+            StringBuilder s = new StringBuilder();
+            foreach (var el in adjacencyList)
+            {
+                s.Clear();
+                el.Value.ForEach(e => s.Append(e.ToString()+" "));
+                yield return String.Format("{0} -> {1}", el.Key, s);
+            }
+        }
+
     }
 
     public class GraphNode
--- buffetTest/Program.cs
+++ buffetTest/Program.cs
@@ -25,18 +25,29 @@ namespace Algoritms
             myLinkedList.Append("second");
             myLinkedList.Append("third");
             myLinkedList.Prepend("zero");// myLinkedList.Reverse();
-            BinarySearchTree binarySearchTree = new BinarySearchTree();
-            binarySearchTree.Insert(9);
-            binarySearchTree.Insert(4);
-            binarySearchTree.Insert(6);
-            binarySearchTree.Insert(20);
-            binarySearchTree.Insert(170);
-            binarySearchTree.Insert(15);
-            binarySearchTree.Insert(1);
-            binarySearchTree.Insert(14);
-            binarySearchTree.Insert(16);
-            binarySearchTree.Remove(20);
-            binarySearchTree.Remove(170);
+
+            MyGraph graph = new MyGraph();
+            graph.addNode(0);
+            graph.addNode(1);
+                graph.addNode(2) ;
+                graph.addNode(3) ;
+                graph.addNode(4) ;
+                graph.addNode(5) ;
+            graph.addNode(6);
+            graph.addEdge(0, 1);
+            graph.addEdge(0, 2);
+            graph.addEdge(3, 1);
+            graph.addEdge(3, 4);
+            graph.addEdge(4, 2);
+            graph.addEdge(4, 5);
+            graph.addEdge(1, 2);
+            graph.addEdge(6, 5);
+            var list = graph.ShowConnections();
+           foreach(var l in list)
+            {
+                Console.WriteLine(l);
+            }
+
 
 
 
