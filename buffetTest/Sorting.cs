2e0e8ce5e01952311d382d28349aeeeb850494fa
MZ <maria.zhukova.vrn@gmail.com>
2019-08-10 12:21:06 +0300

    QuickSort copypasta

--- buffetTest/Sorting.cs
+++ buffetTest/Sorting.cs
@@ -148,5 +148,37 @@ namespace buffetTest
                 }
             }
         }
+        //copyPasta
+        int partition(int[] array, int start, int end)
+        {
+            int temp;//swap helper
+            int marker = start;//divides left and right subarrays
+            for (int i = start; i < end; i++)
+            {
+                if (array[i] < array[end]) //array[end] is pivot
+                {
+                    temp = array[marker]; // swap
+                    array[marker] = array[i];
+                    array[i] = temp;
+                    marker += 1;
+                }
+            }
+            //put pivot(array[end]) between left and right subarrays
+            temp = array[marker];
+            array[marker] = array[end];
+            array[end] = temp;
+            return marker;
+        }
+
+        void quicksort(int[] array, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            int pivot = partition(array, start, end);
+            quicksort(array, start, pivot - 1);
+            quicksort(array, pivot + 1, end);
+        }
     }
 }
