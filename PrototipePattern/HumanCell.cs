06022d9e6434102e089160394c5f57ebe65fabe7
MZ <maria.zhukova.vrn@gmail.com>
2019-08-21 16:33:54 +0300

    PrototypePattern

---
+++ PrototipePattern/HumanCell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class HumanCell : Prototype
+    {
+        string Membrane { get; set; }
+        string Kernel { get; set; }
+
+        public HumanCell(string kernel,string membrane)
+        {
+            Kernel = kernel;
+            Membrane = membrane;
+        }
+        public override Prototype Clone()
+        {
+            return new HumanCell(Kernel, Membrane);
+        }
+
+        public override string GetInfo()
+        {
+            return String.Format("Curent cell with Kernel {0} and Memebrane {1}", Kernel, Membrane);
+        }
+    }
+}
---
+++ PrototipePattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create blood cell");
+            Prototype prototype = new HumanCell("bloodKernel", "bloodMembrane");
+            var cloned = prototype.Clone();
+            Console.WriteLine(prototype.GetInfo());
+            Console.WriteLine("BloodCloned" + cloned.GetInfo());
+            Console.WriteLine("Create skin cell");
+            prototype = new HumanCell("skinKernel", "skinMembrane");
+            var skinCloned = prototype.Clone();
+            Console.WriteLine(prototype.GetInfo());
+            Console.WriteLine("SkinCloned" + skinCloned.GetInfo());
+            Console.ReadKey();
+
+        }
+    }
+}
