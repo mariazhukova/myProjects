4e456cba03a34129e3aa7c0e13930d79cbc8d9dd
MZ <maria.zhukova.vrn@gmail.com>
2019-08-25 14:13:19 +0300

    Test for DecoratorPattern

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Pizza pizza = new ItalianPizza("italian");
+            Pizza pizzaFrench = new FranchPizza("french");
+
+            pizza = new TomatenPizza(pizza);
+            pizzaFrench = new FishPizza(pizzaFrench);
+
+            Console.WriteLine($"{pizza.Name} cost {pizza.GetCost()}");
+            Console.WriteLine($"{pizzaFrench.Name} cost {pizzaFrench.GetCost()}");
+
+            Console.ReadKey();
         }
     }
 }
