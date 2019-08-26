0443056d5d804de81234b8a6549a2c8ca0a0f927
MZ <maria.zhukova.vrn@gmail.com>
2019-08-26 21:23:59 +0300

    SingleNumber + XOR

--- buffetTest/EasyQuestions.cs
+++ buffetTest/EasyQuestions.cs
@@ -4,10 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace buffetTest
+namespace Algoritms
 {
     public class EasyQuestions
     {
+
+        /// <summary>
+        /// Write a function that reverses a string. The input string is given as an array of characters char[].
+        /// Do not allocate extra space for another array, you must do this by modifying 
+        /// the input array in-place with O(1) extra memory.
+        /// </summary>
+        /// <param name="str"></param>
+        public char[] RevertStringWithoutNewArray(char[] str)
+        {
+            int j = 0;
+            int p = str.Length - 1;
+            for (int i = (str.Length - 1) / 2; i >= 0; i--)
+            {
+                char temp = str[j];
+                str[j] = str[p];
+                str[p] = temp;
+                j++;
+                p--;
+            }
+            return str;
+        }
         /// <summary>
         /// Write a program that outputs the string representation of numbers from 1 to n.
         /// But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. 
@@ -45,13 +66,34 @@ namespace buffetTest
         /// <returns></returns>
         public int SingleNumber(int[] nums)
         {
-            List<int> numscopy = nums.ToList();
-            for(int i = 0; i < nums.Length - 1; i++)
+            List<int> numscopy = new List<int>();
+            Parallel.Invoke(() =>
             {
-                if (numscopy.Count(e=>e==nums[i])>1)
-                    numscopy.RemoveAll(e=>e==nums[i]);
-            }
+                for (int i = 0; i <= nums.Length - 1; i++)
+                {
+                    if (numscopy.Contains(nums[i]))
+                        numscopy.Remove(nums[i]);
+                    else numscopy.Add(nums[i]);
+                }
+            });
             return numscopy.ElementAtOrDefault(0);
+        }
+
+        //Bit Manipulation
+        //Concept
+
+        //If we take XOR of zero and some bit, it will return that bit
+        //  a⊕0=a
+        //If we take XOR of two same bits, it will return 0
+        //  a⊕a=0
+        //  a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
+        //So we can XOR all bits together to find the unique number.
+        public int SingleNumberwithXOR(int[] nums)
+        {
+            int x = 0;
+           foreach(int i in nums)
+                x ^= i;
+            return x;
         }
     }
 }
--- buffetTest/Tasks.cs
+++ buffetTest/Tasks.cs
@@ -8,27 +8,6 @@ namespace Algoritms
 {
     class Tasks
     {
-        /// <summary>
-        /// Write a function that reverses a string. The input string is given as an array of characters char[].
-        /// Do not allocate extra space for another array, you must do this by modifying 
-        /// the input array in-place with O(1) extra memory.
-        /// </summary>
-        /// <param name="str"></param>
-        public char[] RevertStringWithoutNewArray(char[] str)
-        {
-            int j = 0;
-            int p = str.Length - 1;
-            for (int i = (str.Length - 1) / 2; i >= 0; i--)
-            {
-                char temp = str[j];
-                str[j] = str[p];
-                str[p] = temp;
-                j++;
-                p--;
-            }
-            return str;
-        }
-
         public int FibonachiIterative(int index)
         {
             int fib = 0;
